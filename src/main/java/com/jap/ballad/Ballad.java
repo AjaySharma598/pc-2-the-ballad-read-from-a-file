074da653426274f25d92a7aac63d9f57b1a12a68
@ajaydixit598 <ajaydixit598@gmail.com>
2022-11-15 16:51:12 +0530

    task completed

--- src/main/java/com/jap/ballad/Ballad.java
+++ src/main/java/com/jap/ballad/Ballad.java
@@ -7,6 +7,16 @@ import java.io.IOException;
 public class Ballad {
     public String readPoemFromAFile(String fileName){
        // Write the logic to read from a file
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            int line=0;
+            while ((line=fileInputStream.read())!=-1){
+                char data=(char) line;
+                System.out.println("data = " + data);
+            }
+        }catch (IOException exception){
+            System.out.println("exception = " + exception);
+        }
         return "";
     }
 
